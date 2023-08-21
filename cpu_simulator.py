b9fa7b07642e812788d2ad4a3f5e722d01e87f29
Rowan <rowjw97@gmail.com>
2023-08-21 22:15:14 +0100

    Implemented basic binary translator

--- cpu_simulator.py
+++ cpu_simulator.py
@@ -1,3 +1,10 @@
+opcode_dict = {
+    "add": 0,
+    "subtract": 1,
+    "lw": 2,
+    "sw": 3
+}
+
 class CPU:
     def __init__(self, control_unit, ALU, register, memory_bus) -> None:
         self.control_unit =  control_unit
@@ -5,14 +12,26 @@ class CPU:
         self.register = register
         self.memory_bus =  memory_bus
 
-    def instruction(instruction):
-        pass
+# Interprets assembly language instruction (opcode, register_1, register_2, destination_register) and converts it to binary.
+    def instruction(self, instruction:str):
+        
+        instruction_array = instruction.split(' ')
+        
+        # We'll deal properly with the opcode later.
+        opcode = instruction_array.pop(0)
+        binary_instructions = []
+        for item in instruction_array:
+            item_int = int(item)
+            binary_instructions.append(bin(item_int))
+
+        # Handles conversion of opcode
+        opcode_bin = bin(opcode_dict[opcode])
+
+        return opcode_bin, binary_instructions
+        
 
 class Control_Unit:
     def __init__(self) -> None:
-        pass
-
-    def split_values(instruction):
         pass
 
     def send_instructions(opcode, operands):
@@ -37,10 +56,14 @@ class Register:
 
 class Memory_Bus:
     def __init__(self, size) -> None:
-        self.memory = [0 for i in range(0, size)]
+        self.memory = [0 for _ in range(0, size)]
     
     def insert(address, value):
         pass
 
     def retrieve(address):
         pass
+
+test = CPU(None, None, None, None)
+test_1 = "add 1 4 8"
+test.instruction(test_1)
