56adb0ee1845524511592bf2c9685a30bd6c724a
Rowan <rowjw97@gmail.com>
2023-08-26 12:22:08 +0100

    Implemented remaining mathematical operations.

--- cpu_simulator.py
+++ cpu_simulator.py
@@ -1,8 +1,14 @@
+# TO-DO:
+# - Implement the remaining opcodes
+# - Create logic for dealing with register/memory addresses in instructions
+
+
+
 opcode_dict = {
     "add": 0,
-    "subtract": 1,
-    "lw": 2,
-    "sw": 3
+    "sub": 1,
+    "mult": 2,
+    "div": 3
 }
 
 class CPU:
@@ -25,6 +31,7 @@ class CPU:
         # Handles conversion of opcode
         opcode_bin = bin(opcode_dict[opcode])
 
+        print(opcode_bin, binary_instructions)
         return opcode_bin, binary_instructions
         
 
@@ -39,6 +46,15 @@ class Control_Unit:
         if opcode == bin(0):
             print("This is an add operation.")
             self.ALU.add(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
+        elif opcode == bin(1):
+            print("This is a subtraction operation.")
+            self.ALU.subtract(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
+        elif opcode == bin(2):
+            print("This is a multiplication operation.")
+            self.ALU.multiply(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
+        elif opcode == bin(3):
+            print("This is a divison operation.")
+            self.ALU.divide(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
 
         # When operation is complete, display state of register and memory.
         self.ALU.register.print_register()
@@ -54,6 +70,24 @@ class ALU:
     def add(self, register_1, register_2, destination_register):
         print(f"Adding {register_1} and {register_2}")
         value = register_1 + register_2
+        print("Passing {} to destination register {}".format(value, destination_register))
+        self.register.insert(destination_register, value)
+
+    def subtract(self, register_1, register_2, destination_register):
+        print(f"Subtracting {register_2} from {register_1}")
+        value = register_1 - register_2
+        print("Passing {} to destination register {}".format(value, destination_register))
+        self.register.insert(destination_register, value)
+
+    def multiply(self, register_1, register_2, destination_register):
+        print(f"Multiplying {register_2} and {register_1}")
+        value = register_1 * register_2
+        print("Passing {} to destination register {}".format(value, destination_register))
+        self.register.insert(destination_register, value)
+
+    def divide(self, register_1, register_2, destination_register):
+        print(f"Dividing {register_1} by {register_2}")
+        value = register_1 / register_2
         print("Passing {} to destination register {}".format(value, destination_register))
         self.register.insert(destination_register, value)
 
@@ -91,6 +125,6 @@ class Memory_Bus:
         print(self.memory)
 
 test_cpu = CPU(Control_Unit(ALU(Register(32), Memory_Bus(32))))
-instructions = test_cpu.instruction('add 3 4 7')
+instructions = test_cpu.instruction('div 100 10 7')
 # print(instructions)
 test_cpu.control_unit.send_instructions(instructions[0], instructions[1])
