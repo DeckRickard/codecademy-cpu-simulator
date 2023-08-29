208fccc503791656862dda26622498a2baa0fb88
Rowan <rowjw97@gmail.com>
2023-08-29 21:52:54 +0100

    Implemented memory addressing

--- cpu_simulator.py
+++ cpu_simulator.py
@@ -1,5 +1,4 @@
 # TO-DO:
-# - Create logic for dealing with register/memory addresses in instructions
 # - Implement a way for instructions to be read from an external file.
 # - Implement error and input checking.
 
@@ -29,8 +28,10 @@ class CPU:
         for item in instruction_array:
             # The below logic handles the removal of brackets, and gives the operand a preceding number to determine if this is a register address, a memory address, or a directly inserted number.
             item_list = [*item]            
+            # Brackets are used to determine a cache address.
             if item_list[0] and item_list[-1] in ('(', ')'):
                 operand = [1, int(item.strip('()'))]
+            # Curly brackets are used to signify a memory address.
             elif item_list[0] and item_list[-1] in ('{', '}'):
                 operand = [2, int(item.strip('{}'))]
             else:
@@ -50,37 +51,57 @@ class Control_Unit:
         self.ALU = ALU
 
 
-    def send_instructions(self, opcode:int, operands:list):
+    def send_instructions(self, opcode:int, bin_operands:list):
         print("Sending instructions to the ALU...\n")
-        if len(operands) == 3:
-            register_1, register_2, destination_register = operands[0], operands[1], operands[2]
-        elif len(operands) == 2:
-            register_1, destination_register = operands[0], operands[1]
-        elif len(operands) == 1:
-            destination_register = operands[0]
         
-        if opcode == bin(0):
-            print("This is an add operation.\n")
-            self.ALU.add(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
-        elif opcode == bin(1):
-            print("This is a subtraction operation.\n")
-            self.ALU.subtract(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
-        elif opcode == bin(2):
-            print("This is a multiplication operation.\n")
-            self.ALU.multiply(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
-        elif opcode == bin(3):
-            print("This is a divison operation.\n")
-            self.ALU.divide(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
-        elif opcode == bin(4):
-            print("This is a save word operation.\n")
-            self.ALU.sw(int(register_1, 2), int(destination_register, 2))
-        elif opcode == bin(5):
-            print("This is a load word operation.\n")
-            self.ALU.lw(int(destination_register, 2))
+        # Save word / load word operations need to be handled differently
+        if int(opcode, 2) in (4, 5):
+            if int(opcode, 2) == 4:
+                print("This is a save word operation.\n")
+                if int(bin_operands[1][0], 2) == 1:
+                    self.ALU.sw(int(bin_operands[0][1], 2), int(bin_operands[1][1], 2), False)
+                elif int(bin_operands[1][0], 2) == 2: 
+                    self.ALU.sw(int(bin_operands[0][1], 2), int(bin_operands[1][1], 2), True)
+            elif int(opcode, 2) == 5:
+                print("This is a load word operation.\n")
+                if int(bin_operands[0][0], 2) == 1:
+                    self.ALU.lw(int(bin_operands[0][1], 2), False)
+                else: 
+                    self.ALU.lw(int(bin_operands[0][1], 2), True)
+
+        # If the operation is mathematical, values may need to be retrieved from cache / memory.
+        else:
+            print("Fetching any required values from cache / memory")
+            register_1, register_2 = self.fetch(bin_operands[0]), self.fetch(bin_operands[1])
+            destination_register = [int(num, 2) for num in bin_operands[2]]
+            print(register_1, register_2, destination_register)
+
+            if opcode == bin(0):
+                print("This is an add operation.\n")
+                self.ALU.add(register_1, register_2, destination_register)
+            elif opcode == bin(1):
+                print("This is a subtraction operation.\n")
+                self.ALU.subtract(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
+            elif opcode == bin(2):
+                print("This is a multiplication operation.\n")
+                self.ALU.multiply(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
+            elif opcode == bin(3):
+                print("This is a divison operation.\n")
+                self.ALU.divide(int(register_1, 2), int(register_2, 2), int(destination_register, 2))
 
         # When operation is complete, display state of register and memory.
         self.ALU.register.print_register()
         self.ALU.memory_bus.print_memory()
+    
+    # This function is used to retrieve values from the cache / memory.
+    def fetch(self, operand):
+        # If the operand is a directly inserted value, the function can end.
+        if int(operand[0], 2) == 0:
+            return [int(item, 2) for item in operand]
+        elif int(operand[0], 2) == 1:
+            return self.ALU.lw(int(operand[1], 2), False)
+        elif int(operand[0], 2) == 2:
+            return self.ALU.lw(int(operand[1], 2), True)
 
 
 class ALU:
@@ -92,32 +113,56 @@ class ALU:
     def add(self, register_1, register_2, destination_register):
         print(f"Adding {register_1} and {register_2}")
         value = register_1 + register_2
-        print("Passing {} to destination register {}".format(value, destination_register))
-        self.register.insert(destination_register, value)
+        if destination_register[0] == 1:
+            print("Passing {} to cache register {}".format(value, destination_register[1]))
+            self.register.insert(destination_register[1], value)
+        elif destination_register[0] == 2:
+            print("Passing {} to memory register {}".format(value, destination_register[1]))
+            self.memory_bus.insert(destination_register[1], value)
+        
 
     def subtract(self, register_1, register_2, destination_register):
         print(f"Subtracting {register_2} from {register_1}")
         value = register_1 - register_2
-        print("Passing {} to destination register {}".format(value, destination_register))
-        self.register.insert(destination_register, value)
+        if destination_register[0] == 1:
+            print("Passing {} to cache register {}".format(value, destination_register[1]))
+            self.register.insert(destination_register[1], value)
+        elif destination_register[0] == 2:
+            print("Passing {} to memory register {}".format(value, destination_register[1]))
+            self.memory_bus.insert(destination_register[1], value)
 
     def multiply(self, register_1, register_2, destination_register):
         print(f"Multiplying {register_2} and {register_1}")
         value = register_1 * register_2
-        print("Passing {} to destination register {}".format(value, destination_register))
-        self.register.insert(destination_register, value)
+        if destination_register[0] == 1:
+            print("Passing {} to cache register {}".format(value, destination_register[1]))
+            self.register.insert(destination_register[1], value)
+        elif destination_register[0] == 2:
+            print("Passing {} to memory register {}".format(value, destination_register[1]))
+            self.memory_bus.insert(destination_register[1], value)
 
     def divide(self, register_1, register_2, destination_register):
         print(f"Dividing {register_1} by {register_2}")
         value = register_1 / register_2
-        print("Passing {} to destination register {}".format(value, destination_register))
-        self.register.insert(destination_register, value)
+        if destination_register[0] == 1:
+            print("Passing {} to cache register {}".format(value, destination_register[1]))
+            self.register.insert(destination_register[1], value)
+        elif destination_register[0] == 2:
+            print("Passing {} to memory register {}".format(value, destination_register[1]))
+            self.memory_bus.insert(destination_register[1], value)
 
-    def sw(self, value, destination_register):
-        pass
+    def sw(self, value, destination_register, to_memory):
+        if to_memory:
+            self.memory_bus.insert(destination_register, value)
+        else:
+            self.register.insert(destination_register, value)
 
-    def lw(self, target_register):
-        pass
+    def lw(self, target_register, from_memory):
+        if from_memory:
+            value = self.memory_bus.retrieve(target_register)
+        else:
+            value = self.register.retrieve(target_register)
+        return value
 
 
 class Register:
@@ -125,12 +170,14 @@ class Register:
         self.register = [0 for i in range(0, register_size)]
 
     def insert(self, address, value):
-        print("Saving value to register at address: {}.".format(address))
+        print("Saving {} to register at address: {}.".format(value, address))
         self.register[address] = value
 
     def retrieve(self, address):
         print("Retrieving value from register at address: {}".format(address))
-        return self.register[address]
+        value = self.register[address]
+        print(value)
+        return value
     
     def print_register(self):
         print("State of register: \n")
@@ -142,12 +189,14 @@ class Memory_Bus:
         self.memory = [0 for _ in range(0, size)]
     
     def insert(self, address, value):
-        print("Saving value to memory at address: {}.".format(address))
+        print("Saving {} to memory at address: {}.".format(value, address))
         self.memory[address] = value
 
     def retrieve(self, address):
         print("Retrieving value from memory at address: {}".format(address))
-        return self.memory[address]
+        value = self.memory[address]
+        print(value)
+        return value
     
     def print_memory(self):
         print("State of Memory: \n")
@@ -155,7 +204,14 @@ class Memory_Bus:
         print('\n')
 
 test_cpu = CPU(Control_Unit(ALU(Register(32), Memory_Bus(32))))
-#instructions = test_cpu.instruction('lw 1')
-#test_cpu.control_unit.send_instructions(instructions[0], instructions[1])
-test_cpu.instruction('add (1) (2) (3)')
+instruction_1 = test_cpu.instruction('sw 5 (1)')
+test_cpu.control_unit.send_instructions(instruction_1[0], instruction_1[1])
+instruction_2 = test_cpu.instruction('lw (1)')
+test_cpu.control_unit.send_instructions(instruction_2[0], instruction_2[1])
+instruction_3 = test_cpu.instruction('sw 5 (2)')
+test_cpu.control_unit.send_instructions(instruction_3[0], instruction_3[1])
+instruction_4 = test_cpu.instruction('lw (2)')
+test_cpu.control_unit.send_instructions(instruction_4[0], instruction_4[1])
+instruction_5 = test_cpu.instruction('add (1) (2) (5)')
+test_cpu.control_unit.send_instructions(instruction_5[0], instruction_5[1])
 
